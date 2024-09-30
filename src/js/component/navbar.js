89a41c614ff8506616615a3fe20f39bea15b7ebd
BernardoNurvia <167910567+BernardoNurvia@users.noreply.github.com>
2024-09-30 19:21:05 +0000

    se crea el navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
+import { frost } from "@cloudinary/url-gen/qualifiers/artisticFilter";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div className="container-fluid">
+			<nav className="navbar d-flex justify-content-between rounded-bottom shadow-lg" style={{backgroundColor:"black"}}>
+				<div className="ms-4">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg" alt="Logo" style={{ width: "90px" }} className="d-inline-block align-text-top" onClick={Navigate("/")}/>
+				</div>
+				<div className=" me-4 dropstart dropdown-center">
+					<button type="button" className="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites
+					</button>
+					<ul className="dropdown-menu">
+						<li><a className="dropdown-item" href="#">People</a></li>
+						<li><a className="dropdown-item" href="#">Vehicles</a></li>
+						<li><a className="dropdown-item" href="#">Planets</a></li>
+						<li><hr className="dropdown-divider"/></li>
+						<li><a className="dropdown-item" href="#">All Favorite</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	);
 };
