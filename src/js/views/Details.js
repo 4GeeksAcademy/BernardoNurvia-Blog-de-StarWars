58e6dcc6f20a0a8ddfa038da5161cb11c31a0257
BernardoNurvia <167910567+BernardoNurvia@users.noreply.github.com>
2024-10-06 10:46:53 +0000

    diagramacion de card details

--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from "react-router-dom";
 import PropTypes from 'prop-types';
+import "../../styles/details.css";
 
 const Details = props => {
     const { id } = useParams();
@@ -22,8 +23,8 @@ const Details = props => {
     return (
         <div className="body-info">
             <div className="header">
-                <div className="img-details">
-                    <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} className="card-img-top" alt="`${person.name}`" />
+                <div className="img-details ">
+                    <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} className="card-img-top img-fluid rounded-start" alt="`${person.name}`" />
                 </div>
                 <div className="text-details">
                     {person ? (
@@ -39,16 +40,16 @@ const Details = props => {
                 </div>
             </div>
 
-            <div className="footer-details">
+            <div >
                 {person ? (
-                    <div>
-                        <p>Height: {person.height}</p>
-                        <p>Mass: {person.mass}</p>
-                        <p>Hair Color: {person.hair_color}</p>
-                        <p>Skin Color: {person.skin_color}</p>
-                        <p>Eye Color: {person.eye_color}</p>
-                        <p>Birth Year: {person.birth_year}</p>
-                        <p>Gender: {person.gender}</p>
+                    <div className="footer-details">
+                        <p>Height:<br/> {person.height}</p>
+                        <p>Mass:<br/>  {person.mass}</p>
+                        <p>Hair Color:<br/>  {person.hair_color}</p>
+                        <p>Skin Color:<br/>  {person.skin_color}</p>
+                        <p>Eye Color:<br/>  {person.eye_color}</p>
+                        <p>Birth Year:<br/>  {person.birth_year}</p>
+                        <p>Gender:<br/>  {person.gender}</p>
                     </div>
                 ) : (
                     <div class="spinner-border text-secondary" role="status">
@@ -56,9 +57,9 @@ const Details = props => {
                     </div>
                 )}
             </div>
-            <div className="d-grid gap-2 col-6 mx-auto">
+            <div className="">
                 <Link to="/">
-                    <button className="btn btn-warning">Go back</button>
+                    <button className="btn btn-warning button">Go back</button>
                 </Link>
             </div>
         </div>
